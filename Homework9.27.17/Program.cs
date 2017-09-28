68ff9304014778879e8ac61503b8d5410162ef60
Shane Moodie <shanepmoodie96@gmail.com>
2017-09-28 10:15:35 -0400

    Initial commit

---
+++ Homework9.27.17/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9._27._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check whether a letter is a vowel or a consonant
+            //Console.WriteLine("Enter a letter.");
+            //char yourLetter = char.Parse(Console.ReadLine());
+
+            //switch (yourLetter)
+            //{
+            //    case 'a':
+            //        Console.WriteLine("This is a vowel");
+            //        break;
+            //    case 'e':
+            //        Console.WriteLine("This is a vowel.");
+            //        break;
+            //    case 'i':
+            //        Console.WriteLine("This is a vowel.");
+            //        break;
+            //    case 'o':
+            //        Console.WriteLine("This is a vowel.");
+            //        break;
+            //    case 'u':
+            //        Console.WriteLine("This is a vowel.");
+            //        break;
+            //}
+
+
+            //else{
+            //    Console.WriteLine("This is a consonant.");
+            //}
+
+
+
+
+            Console.WriteLine("Enter a whole number.");
+            int firstValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number.");
+            int secondValue = int.Parse(Console.ReadLine());
+
+            if (firstValue > secondValue)
+            {
+                Console.WriteLine(firstValue + " is larger the larger value.");
+            }
+            else if (secondValue > firstValue)
+            {
+                Console.WriteLine(secondValue + " is the larger value.");
+            }
+
+            else
+            {
+                Console.WriteLine("Both values are equal.");
+            }
+        
+
+        }
+    }
+}
